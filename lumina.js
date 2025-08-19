efeb901ce22e708827eded3ba0b0fabd788ded6f
rakovskaa <rakovskaa@yahoo.com>
2025-08-19 13:38:28 -0400

    Update lumina.js

--- lumina.js
+++ lumina.js
@@ -22,21 +22,6 @@ alert("Please select at least one product you used.");
 return;
 }
 
-/*const formData = new FormData(form);
-const data = Object.fromEntries(formData.entries());    
-
-console.log(data);
-alert("Thank you for your feedback!");
-
-form.reset();
-form.style.display = "none";
-surveyBtn.style.visibility = "visible";
-
-body.style.backgroundImage = 'url("lumina-bg.jpg")';
-body.style.backgroundSize = 'contain';
-body.style.backgroundRepeat = 'repeat';
-body.style.backgroundPosition = 'center';
-}*/
 });
 
 const closeBtn = document.getElementById("close-form-btn");
